f2c84d87d0d4399a543df879ef22622fc79f3be3
Nusra Mohammed <nusrayasinhy@gmail.com>
2026-06-14 15:50:30 -0400

    fix cov failure

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,4 +1,5 @@
 package com.leszko.calculator;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -6,7 +7,16 @@ public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
      @Test
-     public void testSum() {
+     public void testSum() throws Exception {
           assertEquals(5, calculator.sum(2, 3));
+          assertEquals(10, calculator.sum(7, 3));
+          CalculatorController controller = new CalculatorController();
+
+          java.lang.reflect.Field field =
+             CalculatorController.class.getDeclaredField("calculator");
+          field.setAccessible(true);
+          field.set(controller, calculator);
+
+          assertEquals("5", controller.sum(2, 3));
      }
 }
